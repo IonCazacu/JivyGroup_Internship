21320861b605d05f606b8e575702c1d00880a448
IonCazacu <cazacuion99@yahoo.com>
2023-09-22 23:12:24 +0300

    Added validation for all fields

--- main.js
+++ main.js
@@ -1,56 +1,161 @@
 class RegistrationForm {
     constructor() {
-        const registrationForm = document.getElementById('form');
-        registrationForm.addEventListener('submit', this.handleSubmit);
+        this.registrationForm = document.getElementById('form');
+        this.username = document.getElementById('username');
+        this.email = document.getElementById('email');
+        this.password = document.getElementById('password');
+        this.confirmPassword = document.getElementById('confirmPassword');
 
-        const name = document.getElementById('name');
-        name.addEventListener('blur', this.handleNameValidation);
-
-        const email = document.getElementById('email');
-        email.addEventListener('blur', this.handleEmailValidation);
-
-        const formInputPassword = document.getElementById('form-input-password');
-        const password = document.getElementById('password');
-        
-        const formInputConfirmPassword = document.getElementById('form-input-confirm-password');
-        const confirmPassword = document.getElementById('confirm-password');
-
-        const error = document.getElementById('error');
-        
-        const eyeFillBtn = document.getElementById('eye-fill-btn');
-        const eyeSlashFillBtn = document.getElementById('eye-slash-fill-btn');
+        this.registrationForm.addEventListener(
+            'submit',
+            this.handleSubmit.bind(this)
+        );
     }
-
-    handleNameValidation($event) {
-        
-    };
-
-    handleEmailValidation($event) {
-        $event.preventDefault();
-        const emailRegex = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
-        const emailValue = $event.target.value;
-        if (emailValue.trim() === '') {
-            email.classList.add('error');
-            error.innerHTML = 'Email is required';
-            return;
-        }
-
-        if (!emailValue.match(emailRegex)) {
-            error.innerHTML = 'Invalid email address';
-            return;
-        }
-    };
-
+    
     handleSubmit($event) {
         $event.preventDefault();
         console.log('object');
+        this.handleNameValidation();
+        this.handleEmailValidation();
+        this.handlePasswordValidation();
+        this.handleConfirmPasswordValidation();
     };
 
-    onBlur($event) {
-        $event.preventDefault();
-        const target = $event.target.value;
-        console.log(target);
-        const name = target.name;
-        const value = target.value;
-    };
+    handleNameValidation() {
+        let returnMessage = {
+            isValid: true,
+            message: null
+        };
+
+        const isEmpty = this.isEmpty(this.username);
+        
+        if (isEmpty) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Username is required';
+            console.log('handleNameValidation', returnMessage);
+            return returnMessage;
+        }
+        
+        const minLength = 3, maxLength = 18;
+        const isBetween = this.isBetween(this.username, minLength, maxLength);
+
+        if (!isBetween) {
+            returnMessage.isValid = false;
+            returnMessage.message = `Username must be between ${ minLength } and ${ maxLength }`;
+        }
+
+        console.log('handleNameValidation', returnMessage);
+        return returnMessage;
+    }
+
+    handleEmailValidation() {
+        /*
+            ^: Start of the string.
+            
+            [a-zA-Z0-9._%+-]+: Match one or more of the following characters: letters (both upper and lower case), digits, dots, underscores, percent signs, plus signs, and hyphens.
+            
+            @: Match the at symbol.
+            
+            [a-zA-Z0-9.-]+: Match one or more of the following characters: letters (both upper and lower case), digits, dots, and hyphens. This is for the domain name.
+            
+            \.: Match a literal dot.
+            
+            [a-zA-Z]{2,}: Match at least two or more letters. This is for the top-level domain (like com, net, org, etc.).
+            
+            $: End of the string.
+        */
+        const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        
+        let returnMessage = {
+            isValid: true,
+            message: null
+        };
+        
+        const isEmpty = this.isEmpty(this.email);
+
+        if (isEmpty) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Email is required';
+            console.log('handleEmailValidation', returnMessage);
+            return returnMessage;
+        }
+
+        const isMatch = this.email.value.trim().match(EMAIL_REGEX);
+
+        if (!isMatch) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Invalid email address';
+        }
+
+        console.log('handleEmailValidation', returnMessage);
+        return returnMessage;
+    }
+
+    handlePasswordValidation() {
+        let returnMessage = {
+            isValid: true,
+            message: null
+        };
+
+        const isEmpty = this.isEmpty(this.password);
+
+        if(isEmpty) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Password is empty';
+            console.log('handlePasswordValidation', returnMessage);
+            return returnMessage;
+        }
+
+        const minLength = 8, maxLength = 25;
+        const isBetween = this.isBetween(this.password, minLength, maxLength);
+
+        if (!isBetween) {
+            returnMessage.isValid = false;
+            returnMessage.message = `Password must be between ${ minLength } and ${ maxLength }`;
+        }
+
+        console.log('handlePasswordValidation', returnMessage);
+        return returnMessage;
+    }
+
+    handleConfirmPasswordValidation() {
+        let returnMessage = {
+            isValid: true,
+            message: null
+        };
+
+        const passwordIsEmpty = this.isEmpty(this.password);
+        const confirmPasswordIsEmpty = this.isEmpty(this.confirmPassword);
+
+        if(confirmPasswordIsEmpty) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Confirm your password';
+            console.log('handleConfirmPasswordValidation', returnMessage);
+            return returnMessage;
+        }
+
+        if(passwordIsEmpty) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'Password is empty';
+            console.log('handleConfirmPasswordValidation', returnMessage);
+            return returnMessage;
+        }
+
+        if (this.password !== this.confirmPassword) {
+            returnMessage.isValid = false;
+            returnMessage.message = 'The passwords must match';
+        }
+
+        console.log('handleConfirmPasswordValidation', returnMessage);
+        return returnMessage;
+    }
+
+    isBetween(input, minLength, maxLength) {
+        const inputValue = input.value.trim().length;
+        return !this.isEmpty(input) && inputValue >= minLength && inputValue <= maxLength;
+    }
+
+    isEmpty(input) {
+        return !input.value.trim().length;
+    }
 }
